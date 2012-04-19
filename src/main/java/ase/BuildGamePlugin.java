832bcd01845438c260e162bc49b19c585e03fe86
Talon Daniels <talondaniels@gmail.com>
2012-04-18 22:15:53 -0600

    Added getSonarUrl

--- src/main/java/ase/BuildGamePlugin.java
+++ src/main/java/ase/BuildGamePlugin.java
@@ -2,6 +2,10 @@ package ase;
 
 import hudson.Extension;
 import hudson.Launcher;
+import hudson.maven.AbstractMavenProject;
+import hudson.maven.ModuleName;
+import hudson.maven.MavenModule;
+import hudson.maven.MavenModuleSet;
 import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -24,6 +28,9 @@ public class BuildGamePlugin extends Notifier
 	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
 	{
 		listener.getLogger().println("Executing BuildGame plugin...");
+		
+		String url = getSonarUrl(build.getProject());
+		listener.getLogger().println("getSonarUrl returned: " + url);
 		//do stuff
 		int pointValue = ComputePoints.getPointValue("com.deflexicon.bot:SkypeBot", "http://192.168.1.128:9000", "sonar", "sonar");
 		listener.getLogger().println("That build was worth " + pointValue + " points.");
@@ -31,13 +38,33 @@ public class BuildGamePlugin extends Notifier
 		listener.getLogger().println("Finished BuildGame plugin execution.");
 		return true;
 	}
+	
+	private String getSonarUrl(AbstractProject<?,?> project)
+	{
+		//SonarInstallation sonarInstallation = getInstallation();
+		String url = "";
+		if(project instanceof AbstractMavenProject)
+		{
+			 AbstractMavenProject mavenProject = (AbstractMavenProject) project;
+			 if(mavenProject.getRootProject() instanceof MavenModuleSet)
+			 {
+				 MavenModuleSet mms = (MavenModuleSet) mavenProject.getRootProject();
+				 MavenModule rootModule = mms.getRootModule();
+				 if(rootModule != null)
+				 {
+					 ModuleName moduleName = rootModule.getModuleName();
+					 url += moduleName.groupId + ":" + moduleName.artifactId;
+				 }
+			 }
+		}
+		return url;
+	}
 
 	@Extension(ordinal=999)
 	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher>
 	{
 		public boolean isApplicable(Class<? extends AbstractProject> aClass)
 		{
-			// Indicates that this builder can be used with all kinds of project types
 			return true;
 		}
 
